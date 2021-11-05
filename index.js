28358ebf3ac4a8e89eb82f6ef36135512c72c2a2
vagrant <aalibarre@gmail.com>
2021-11-05 02:52:08 +0000

    spotter-2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+const  fetchCoordsByIP = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -8,3 +9,16 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+
+
+
+fetchCoordsByIP('99.247.15.231',(error,data)=>{
+  if (error) {
+    console.log('It did not work.',error);
+    return;
+  }
+    
+  console.log('It worked sending data.',data);
+    
+});
--- iss.js
+++ iss.js
@@ -1,25 +1,49 @@
 const request = require("request");
 
-const fetchMyIP = function(callback) {
+// const fetchMyIP = function(callback) {
+//   request(
+//     `https://api.ipify.org?format=json`,
+//     function(error, response, body) {
+//       if (error) {
+//         callback(error);
+//         return;
+//       }
+//       const ipData = JSON.parse(body).ip;
+//       console.log(ipData);
+//       if (response.statusCode !== 200) {
+//         const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//         callback(Error(msg), null);
+//         return;
+//       }
+//     }
+//   );
+// };
+   
+
+
+const fetchCoordsByIP = function(ip, callback) {
   request(
-    `https://api.ipify.org?format=json`,
+    `https://freegeoip.app/json/${ip}`,
     function(error, response, body) {
       if (error) {
-        callback(error);
-        return;
+        callback(error,null);
       }
-      const ipData = JSON.parse(body).ip;
-      console.log(ipData);
+          
       if (response.statusCode !== 200) {
-        const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+        const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
         callback(Error(msg), null);
         return;
+      }
+      // if is-200 status, then parse through the body
+      if (response.statusCode) {
+        const { latitude, longitude } = JSON.parse(body);
+        callback(null, {latitude, longitude});
       }
     }
   );
 };
-  
-module.exports = { fetchMyIP };
+
+module.exports =  fetchCoordsByIP;
 
 
 
